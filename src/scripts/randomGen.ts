99abf99db1b8a8403bfd8f1dec425b75570a0c1d
Max Becker <becker.maxh@gmail.com>
2020-05-23 15:41:11 -0700

    type randomGen

--- src/scripts/randomGen.ts
+++ src/scripts/randomGen.ts
@@ -1,10 +1,10 @@
-export function randomGen(min, max) {
+export function randomGen(min: number, max: number): number {
   // inclusive of max
   const scale = max - min;
   return min + scale * Math.random();
 }
 
-export function randomGenInt(min, max) {
+export function randomGenInt(min: number, max: number) {
   // inclusive of max
   return Math.round(randomGen(min - .5, max + .5));
 }
