00ee89ab6a53826356edfe0d2246094f019a81f7
Max Becker <becker.maxh@gmail.com>
2020-05-26 04:08:28 -0700

    work on chess game

--- src/components/ChessGame/main.tsx
+++ src/components/ChessGame/main.tsx
@@ -1,6 +1,15 @@
 import React from 'react'
+import useStyles from './style'
 import Board from './Board'
+import { RouteComponentProps } from '@reach/router'
 
-function ChessGame (){
-
+function ChessGame(props: RouteComponentProps) {
+  const classes = useStyles()
+  return (
+    <div className={classes.Bounder}>
+      <Board />
+    </div>
+  )
 }
+
+export default ChessGame
