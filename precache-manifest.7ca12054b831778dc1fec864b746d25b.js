580a118d3d75e9b36ef42e0fedb0575d9e975d36
Max Becker <becker.maxh@gmail.com>
2020-05-28 16:13:57 -0700

    Updates

---
+++ precache-manifest.7ca12054b831778dc1fec864b746d25b.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d947bb053b8d27a2e0b1a20eba8deefc",
+    "url": "/myreact-ts/index.html"
+  },
+  {
+    "revision": "b4f931e53a2d0a577425",
+    "url": "/myreact-ts/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e7ea4217821078d595fa",
+    "url": "/myreact-ts/static/js/2.92f65c7f.chunk.js"
+  },
+  {
+    "revision": "e53763128d977c2aa839cd529c578e9f",
+    "url": "/myreact-ts/static/js/2.92f65c7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4f931e53a2d0a577425",
+    "url": "/myreact-ts/static/js/main.8ea3413f.chunk.js"
+  },
+  {
+    "revision": "6472290610e6a3cff3e8",
+    "url": "/myreact-ts/static/js/runtime-main.317d3ef5.js"
+  },
+  {
+    "revision": "8b5c63b17ccd91534c8bb05db2c45a9f",
+    "url": "/myreact-ts/static/media/blackBishop.8b5c63b1.svg"
+  },
+  {
+    "revision": "a8e5469044ef17e4433283633d5ee7eb",
+    "url": "/myreact-ts/static/media/blackKing.a8e54690.svg"
+  },
+  {
+    "revision": "dd7d65b0547336bcf9750d0bfa2f22c5",
+    "url": "/myreact-ts/static/media/blackKnight.dd7d65b0.svg"
+  },
+  {
+    "revision": "efb79a63a7af9dc1d8aab61d0f06f6ad",
+    "url": "/myreact-ts/static/media/blackPawn.efb79a63.svg"
+  },
+  {
+    "revision": "9beb2c2914f7ec7c744eb19412d0364b",
+    "url": "/myreact-ts/static/media/blackQueen.9beb2c29.svg"
+  },
+  {
+    "revision": "490d08910334517de46613afe8411a45",
+    "url": "/myreact-ts/static/media/blackRook.490d0891.svg"
+  },
+  {
+    "revision": "19c4b7eb320900bc493075b5855763eb",
+    "url": "/myreact-ts/static/media/whiteBishop.19c4b7eb.svg"
+  },
+  {
+    "revision": "99a07504301234ee9a3dfaf386330f6d",
+    "url": "/myreact-ts/static/media/whiteKing.99a07504.svg"
+  },
+  {
+    "revision": "4a32b3c46ffe64a07c00cf3eac0e239e",
+    "url": "/myreact-ts/static/media/whiteKnight.4a32b3c4.svg"
+  },
+  {
+    "revision": "22ecec5fcf7f6f0ccd7fe3dffa12f307",
+    "url": "/myreact-ts/static/media/whitePawn.22ecec5f.svg"
+  },
+  {
+    "revision": "7d26a45005568a2d47c0dcabd3d0fac2",
+    "url": "/myreact-ts/static/media/whiteQueen.7d26a450.svg"
+  },
+  {
+    "revision": "81a8c280ad6c2d7d0bc8c0fd78db2519",
+    "url": "/myreact-ts/static/media/whiteRook.81a8c280.svg"
+  }
+]);
